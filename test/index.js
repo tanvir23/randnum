19cabd18d96836e50e5633b36db3189365355781
Tanvir <tanvir@nltvc.com>
2018-08-02 17:51:36 +0800

    developing makeMYPhone feature and adding unit test for it

--- test/index.js
+++ test/index.js
@@ -1,4 +1,21 @@
-import assert from 'assert';
+import * as assert from 'assert';
+import maskMYPhone from '../src';
+
+const testSamples = [
+    { input: 'abc', expectedResult: 'abc', description: 'should return pristine value when receiving "abc"' },
+    { input: 'abc1234567', expectedResult: 'abc1234567', description: 'should return pristine value when receiving "abc1234567"' },
+    { input: 'abcdefghij', expectedResult: 'abcdefghij', description: 'should return pristine value when receiving "abcdefghij"' },
+    { input: '1234567890', expectedResult: '(123) 456-7890', description: 'should return (123) 456-7890' },
+    { input: '5431260987', expectedResult: '(543) 126-0987', description: 'should return (543) 126-0987' },
+];
+
+describe('Array', () => {
+    testSamples.forEach((sample) => {
+        it(sample.description, () => {
+            assert.equal(maskMYPhone(sample.input), sample.expectedResult);
+        });
+    });
+});
 
 describe('Array', () => {
     describe('#indexOf()', () => {
